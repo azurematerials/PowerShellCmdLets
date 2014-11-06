300fb68ac32b4d7c57f22f8371462ae11df1afb3
Jeff Chu <regionbbs@gmail.com>
2014-11-06 16:25:28 +0800

    add azure login with publish settings

---
+++ AzureAccountLoginByPublishSettingsFile.ps1
@@ -0,0 +1,8 @@
+﻿# download Azure publish setting file.
+Get-AzurePublishSettingsFile
+# import Azure publish setting file.
+Import-AzurePublishSettingsFile "[YOUR_AZURE_PUBLISH_SETTINGS]"
+# get Azure subscriptions.
+Get-AzureSubscription
+# select Azure subscription for next operation session.
+Select-AzureSubscription -SubscriptionName "[SUBSCRIPTION_NAME]"
